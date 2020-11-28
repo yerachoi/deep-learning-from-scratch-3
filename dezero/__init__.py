224ee37f55bc5604006574d55dd89b07aa421101
Yera Choi <yeracachoi@gmail.com>
2020-11-28 21:55:51 +0900

    update steps 44, 45 (add layers & models)

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -12,12 +12,16 @@ if is_simple_core:
 else:
     from dezero.core import Variable
     from dezero.core import Function
+    from dezero.core import Parameter
     from dezero.core import using_config
     from dezero.core import no_grad
     from dezero.core import as_array
     from dezero.core import as_variable
     from dezero.core import setup_variable
-    
+
+    from dezero.layers import Layer
+    from dezero.models import Model
+
     import dezero.functions
     import dezero.utils
 
