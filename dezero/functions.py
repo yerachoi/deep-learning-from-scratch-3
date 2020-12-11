f5e68e6a87808740521e2de2cb21587b515abd39
Yera Choi <yeracachoi@gmail.com>
2020-12-11 14:10:00 +0900

    update step 50 (on DataLoader & accuracy)

--- dezero/functions.py
+++ dezero/functions.py
@@ -442,3 +442,12 @@ class Clip(Function):
 
 def clip(x, x_min, x_max):
     return Clip(x_min, x_max)(x)
+
+
+def accuracy(y, t):
+    y, t = as_variable(y), as_variable(t)
+
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
