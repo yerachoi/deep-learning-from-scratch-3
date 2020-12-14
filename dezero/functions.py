e1af6ee38c044702fc3386769c4bb4921e6cc8fc
Yera Choi <yeracachoi@gmail.com>
2020-12-14 15:22:35 +0900

    update step 51 (on mnist, relu, adam)

--- dezero/functions.py
+++ dezero/functions.py
@@ -321,6 +321,23 @@ def softmax_simple(x, axis=1):
     sum_y = sum(y, axis=axis, keepdims=True)
     return y / sum_y
 
+
+class ReLU(Function):
+    def forward(self, x):
+        y = np.maximum(x, 0.0)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        mask = x.data > 0
+        gx = gy * mask
+        return gx
+
+
+def relu(x):
+    return ReLU()(x)
+
+
 # =============================================================================
 # loss function: mean_squared_error / softmax_cross_entropy / sigmoid_cross_entropy / binary_cross_entropy
 # =============================================================================
